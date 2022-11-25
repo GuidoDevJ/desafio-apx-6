78b1e5980b0e53c4683240f5969f31ff2a79f653
Guido <[guidogauna9@gmail.com]>
2022-11-24 23:26:14 -0300

    fourt

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,10 @@
+import {nanoid} from "nanoid"
+import * as express from "express"
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
 dotenv.config()
+const app = express()
+
+app.use(express.json())
 
 console.log(process.env.rtdbKey)
